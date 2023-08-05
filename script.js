7da2e5b8f222f2a19266ed673370ab30c29c9fa3
Felipe Schiavinatto Costa <schiavinatto.felipe@gmail.com>
2023-08-05 03:15:02 -0300

    Alteração no código javascript para manipular o DOM de forma mais eficaz e semanticamente mais correta.

--- script.js
+++ script.js
@@ -168,13 +168,30 @@ function calcularVerbas(){
     if(botao_com_justa_causa.checked){
         secaoResultado.style.display = 'flex';
 
+        ocultarResult13salario.style.display = 'none';
+        ocultarResultferias.style.display = 'none';
+        ocultarResultMulta.style.display = 'none'; 
+        ocultarResultFGTS.style.display = 'none'; 
+
+      
         
 
-        resultadoFinal.innerHTML = "<div>Seu saldo de salário é: R$ " + saldoSalario + "</div>"+
-        "<div><p>Considerando a demissão por justa causa, você tem direito ao seu saldo de salário, somente, bem como, férias vencidas se for o caso.</p><p>No entanto, é importante lembrar que você também terá direito a receber o 1/3 constitucional de férias vencidas, caso possua, salário família se for o caso e as suas horas extras caso haja.</p><p>Para isso, busque a ajuda de um advogado especialista que poderá te auxiliar e dizer com certeza os seus direitos.</p></div>"
+        
+        saldoSalarioResult.innerHTML = "R$ " + saldoSalario
+        rescisaoTotalResult.innerHTML = "R$ " + saldoSalario
+        explicacao.innerHTML = "<p>Considerando a demissão por justa causa, você tem direito ao seu saldo de salário, somente, bem como, férias vencidas se for o caso.</p><p>No entanto, é importante lembrar que você também terá direito a receber o 1/3 constitucional de férias vencidas, caso possua, salário família se for o caso e as suas horas extras caso haja.</p><p>Para isso, busque a ajuda de um advogado especialista que poderá te auxiliar e dizer com certeza os seus direitos.</p>"
     }
     else if(botao_sem_justa_causa.checked && pediDemissao.checked){
+
+
         secaoResultado.style.display = 'flex';
+
+
+        ocultarResult13salario.style.display = 'block';
+        ocultarResultferias.style.display = 'block';
+        ocultarResultMulta.style.display = 'none'; 
+        ocultarResultFGTS.style.display = 'none'; 
+       
 
         
        
@@ -183,16 +200,24 @@ function calcularVerbas(){
         
         
 
-        resultadoFinal.innerHTML = "<div>Seu saldo de salário é: R$ " + saldoSalario + "</div>" + 
-        "<div>13º salário porporcional: R$ " + salario13 + "</div>" +
-        "<div>Férias proporcionais mais 1/3: R$ " + ferias + "</div>"+
-        "<div>Valor total da rescisão: R$ " + rescisaoTotal + "</div>" +
-        "<div><p>É importante lembrar que quando o empregado pede demissão, ele deve cumprir o aviso prévio e receber o salário equivalente, podendo ser dispensado pela empresa de cumprir esse aviso, total ou parcial. Neste último caso, o empregado recebe só pelos dias em que trabalhou.</p><p>Vale lembrar, no entanto, que caso o empregado comprove que arrumou um novo emprego, a empresa é obrigada a liberá-lo do aviso prévio.</p><p>Além disso, quando o empregado é quem pede demissão, ele não terá direito a sacar seu FGTS, nem receber a multa de 40%.</p><p>Por fim, é importante destacar que o empregado, neste caso, também não receberá o seguro desemprego.</p></div>"
+        saldoSalarioResult.innerHTML = "R$ " + saldoSalario
+        salario13Result.innerHTML = "R$ " + salario13
+        feriasResult.innerHTML = "R$ " + ferias
+        rescisaoTotalResult.innerHTML = "R$ " + rescisaoTotal
+        
+
+        explicacao.innerHTML = "<p>É importante lembrar que quando o empregado pede demissão, ele deve cumprir o aviso prévio e receber o salário equivalente, podendo ser dispensado pela empresa de cumprir esse aviso, total ou parcial. Neste último caso, o empregado recebe só pelos dias em que trabalhou.</p><p>Vale lembrar, no entanto, que caso o empregado comprove que arrumou um novo emprego, a empresa é obrigada a liberá-lo do aviso prévio.</p><p>Além disso, quando o empregado é quem pede demissão, ele não terá direito a sacar seu FGTS, nem receber a multa de 40%.</p><p>Por fim, é importante destacar que o empregado, neste caso, também não receberá o seguro desemprego.</p>"
     }
 
     else if(botao_sem_justa_causa.checked && fuiDemitido.checked){
         secaoResultado.style.display = 'flex';
 
+        ocultarResult13salario.style.display = 'block';
+        ocultarResultferias.style.display = 'block';
+        ocultarResultMulta.style.display = 'block'; 
+        ocultarResultFGTS.style.display = 'block'; 
+
+      
      
        
         rescisaoTotal = (saldoSalario + salario13 + ferias + multaFgts).toFixed(2);
@@ -200,13 +225,16 @@ function calcularVerbas(){
         
 
 
-        resultadoFinal.innerHTML = "<div>Seu saldo de salário é: R$" + saldoSalario + "</div>" + 
-        "<div>13º salário porporcional: R$ " + salario13 + "</div>" +
-        "<div>Férias proporcionais mais 1/3: R$ " + ferias + "</div>"+
-        "<div>Seu FGTS é: R$ " + FGTS + "</div>" +
-        "<div> Sua multa de 40% do FGTS é: R$ " + multaFgts + "</div>" +
-        "<div>Valor total da rescisão: R$ " + rescisaoTotal + "</div>" +
-        "<div><p>É importante lembrar que nesse caso o empregado ainda terá direito ao aviso prévio, seja ele trabalhado ou indenizado, bem como, receber suas férias vencidas mais 1/3 se for o caso</p><p>Além disso, neste caso o empregado também terá direito a sacar seu FGTS e receber o seguro desemprego.</p></div>"
+        saldoSalarioResult.innerHTML = "R$ " + saldoSalario
+        salario13Result.innerHTML = "R$ " + salario13
+        feriasResult.innerHTML = "R$ " + ferias
+        multaFGTSresult.innerHTML = "R$ " + multaFgts
+        saldoFGTSresult.innerHTML = "R$ " + FGTS + " "
+        rescisaoTotalResult.innerHTML = "R$ " + rescisaoTotal
+
+
+
+        explicacao.innerHTML = "<p>É importante lembrar que nesse caso o empregado ainda terá direito ao aviso prévio, seja ele trabalhado ou indenizado, bem como, receber suas férias vencidas mais 1/3 se for o caso</p><p>Além disso, neste caso o empregado também terá direito a sacar seu FGTS e receber o seguro desemprego.</p></div>"
 
     }
 }
